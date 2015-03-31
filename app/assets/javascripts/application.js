073c1781b8fa9830fc972a56745883e15962a9dd
Paul Resende <paulresende@me.com>
2015-04-01 00:49:51 +0100

    redoing styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,8 +18,39 @@
 
 /// modal view advert ///
 
+$(document).ready(function(){
+  
+  // NAVIGATION MENU
+  
+  // menu icon states, opening main nav
+  $('#menu-icon').click(function(){
+    $(this).toggleClass('open');
+    $('#menu,#menu-toggle,#page-content,#menu-overlay').toggleClass('open');
+    $('#menu li,.submenu-toggle').removeClass('open');
+    $('#menu li').removeClass('disabled');
+  });
+  
+  // clicking on overlay closes menu
+  $('#menu-overlay').click(function(){
+    $('*').removeClass('open');
+    $('*').removeClass('disabled');
+  });
+  
+  // add child menu toggles and parent class
+  $('#menu li').has('ul').addClass('parent').prepend('<div class="submenu-toggle">open</div>');
+  
+  // toggle child menus
+  $('.submenu-toggle').click(function(){
+    var currentToggle=$(this);
+    currentToggle.parent().siblings().toggleClass('disabled');
+    currentToggle.parent().toggleClass('open');
+    currentToggle.toggleClass('open');
+  });
+});
 
 /// new bid form ///
+
+
 
 $(document).ready(function() {
      $("body").css("display", "none");
@@ -127,8 +158,5 @@ $(document).ready(function() {
     console.log("button clicked!")
   });  
 
-
-
-  
-
+ 
 });
